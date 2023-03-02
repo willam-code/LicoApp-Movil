880a3637560817a6650335569182dceb20b0f836
willam <dgutierrezvidaurre@gmail.com>
2023-03-02 18:00:26 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Licoreria-Movil',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
